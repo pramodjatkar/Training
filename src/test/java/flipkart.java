aa213b621d41c971e7483f28a564cb2e7c3adf7e
pramodjatkar <pramodjatkar1234@gmail.com>
2022-03-29 21:29:46 +0530

    adding flipkart code

--- src/test/java/flipkart.java
+++ src/test/java/flipkart.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.ArrayList;
+
 public class flipkart {
     private String testUrl;
     private WebDriver driver;
@@ -29,10 +31,10 @@ public class flipkart {
         // driver.navigate().to(testUrl);
         driver.get(testUrl);
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input")).sendKeys("7619663345");
-        Thread.sleep(10000);
+        Thread.sleep(2000);
 
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/input")).sendKeys("bannanje123");
-        Thread.sleep(10000);
+        Thread.sleep(2000);
 
         driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div/div[2]/div/form/div[4]/button")).click();
 
@@ -40,17 +42,56 @@ public class flipkart {
 
     @Test
     public  void  testExample() throws InterruptedException {
-        Thread.sleep(5000);
-        driver.findElement(By.className("_3704LK")).sendKeys("mi mobile");
-        Thread.sleep(5000);
+        Thread.sleep(1000);
+        driver.findElement(By.className("_3704LK")).sendKeys("mobile");
+        Thread.sleep(1000);
         driver.findElement(By.xpath("//html/body/div[1]/div/div[1]/div[1]/div[2]/div[2]/form/div/button")).click();
 
         Thread.sleep(3000);
 
-        driver.findElement(By.xpath("/html/body/div/div/div[3]/div[1]/div[2]/div[2]")).click();
+        driver.findElement(By.xpath("/html/body/div/div/div[3]/div[1]/div[2]/div[3]")).click();
+
+
+        ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(0));
+        driver.close();
+        driver.switchTo().window(tabs2.get(1));
+        Thread.sleep(2000);
+        try
+        {
+            driver.navigate();
+            driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[1]/button")).click();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+        }
+
+        driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div[1]/div[2]/div[5]/div/div/a")).click();
+
+        Thread.sleep(5000);
+
+        driver.findElement(By.cssSelector("._2KpZ6l")).click();
+
+//        driver.findElement(By.xpath("#container > div > div._1eztQ7 > div > div > div:nth-child(2) > div > div > div > div > div > label > div._2jIO64 > div > form > div > div:nth-child(2) > div:nth-child(1) > input")).sendKeys("Pramod");
+//
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/label/div[2]/div/form/div/div[2]/div[2]/input")).sendKeys("7619663345");
+//
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/label/div[2]/div/form/div/div[3]/div[1]/input")).sendKeys("586102");
+//
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/label/div[2]/div/form/div/div[3]/div[2]/input")).sendKeys("Vijayapur");
+//
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/label/div[2]/div/form/div/div[4]/div/div[1]/textarea")).sendKeys("near rto, Vijayapur");
+//
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/label/div[2]/div/form/div/div[5]/div[1]/div[1]/input")).sendKeys("Vijayapur");
+//
+//        Select dropState = new Select(driver.findElement(By.name("state")));
+//        dropState.selectByVisibleText("Karnataka");
+//
+//        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/label/div[2]/div/form/div/div[6]/div[1]/input")).sendKeys("near Rto");
 
         Thread.sleep(3000);
-        driver.findElement(By.xpath("/html/body/div[1]/div/div[3]/div[1]/div[1]/div[2]/div/ul/li[1]")).click();
+        driver.findElement(By.xpath("/html/body/div/div/div[2]/div/div[1]/div[2]/div/div/div/div/div/label/div[2]/div/form/div/div[1]/button")).click();
 
 
     }
